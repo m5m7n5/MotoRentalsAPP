364b0680dc334b07ef2966471a35e8ec479cae1c
Cantor <Cantor@192.168.1.42>
2015-12-12 12:01:51 +0100

    LogIn

--- src/Controlador/Motorentals.java
+++ src/Controlador/Motorentals.java
@@ -53,6 +53,115 @@ public class Motorentals {
         this.showUserMenu();
     }
     
+    /*-------------------------------------------------------------*/
+    /* -------------------- LogIn & SignUp ------------------------*/
+    /*-------------------------------------------------------------*/
+    
+    private void logIn(){
+        boolean check;
+        boolean exists = false;
+        int tries = 0;
+        String user, pass;
+        Person pers = null;
+        
+        /**
+         * Trying to select the person by its username.
+         * Maximum number of tries: 3.
+         */
+        
+        while (!exists & tries < 3){
+            int i;
+            Consola.escriu("Insert username: ");
+            user = Consola.llegeixString();
+            
+            //Checking: is our user a client?
+            //We made this for because we want to get out if we found the client.
+            for (i = 0; i < lstClient.size() & !exists; i++){
+                Client c = lstClient.get(i);
+                check = c.compareByUser(user);
+                if (check){
+                    exists = true;
+                    pers = c;
+                }
+            }
+            
+            //Checking: is our user a manager?
+            //Since we made this kind of for loop, if we already found a person we won't enter here.
+            for (i = 0; i < lstManager.size() & !exists; i++){
+                Manager m = lstManager.get(i);
+                check = m.compareByUser(user);
+                if (check){
+                    exists = true;
+                    pers = m;
+                }
+            }
+            
+            //Checking: is our user an admin?
+            //Same situation as before.
+            for (i = 0; i < lstAdmin.size() & !exists; i++){
+                Admin a = lstAdmin.get(i);
+                check = a.compareByUser(user);
+                if (check){
+                    exists = true;
+                    pers = a;
+                }
+            }
+            
+            //If we don't find anything, then the username was incorrect.
+            if (!exists){
+                Consola.escriu("Invalid username, please try again.");
+                tries +=1;
+            }
+        }
+        
+        /**
+         * If we reached the end, we wither have a person or we have more than 3 tries.
+         * More than 3 tries means that exists = false.
+         * Let's ask for the password in the case that exists is true.
+         */
+        
+        if (exists){
+            exists = false;
+            tries = 0; //You can try 3 times for your password.
+            
+            while (!exists & tries < 3){
+                Consola.escriu("Insert password: ");
+                pass = Consola.llegeixString();
+            
+                check = pers.compareByPassword(pass);
+            
+                if (check){
+                    exists = true;
+                    Consola.escriu("Correct password");
+                } else {
+                    Consola.escriu("Incorrect password.");
+                    tries +=1;
+                }
+            }
+        }
+        
+        /**
+         * We can have two situations: either we have a correct person (with correct user and password)
+         * or we have exceeded the maximum tries permited.
+         * If we have a correct person, we show the pertinent menu.
+         * If we reach here with more than 3 tries, then you can't log in.
+         */
+        
+        if (pers instanceof Client & tries < 3){
+            showClientMenu();
+            current = pers; //This will be our current user
+        } else if (pers instanceof Manager & tries < 3){
+            showManagerMenu();
+            current = pers; 
+        } else if (pers instanceof Admin & tries < 3){
+            showAdminMenu(); 
+            current = pers;
+        } else {
+            Consola.escriu("You've exceeded the maximum permited tries");
+        }
+           
+    }
+    
     
     /*------------------------------------------------------------*/
     /* --------------   Menus and Select Options -----------------*/
@@ -203,8 +312,11 @@ public class Motorentals {
                 Consola.escriu("Log out");
                 break;
         }
-        
+                
     }
+    
+
+    
     public int checkNumber(int index, int max){
         boolean correct = false;
         while (!correct){
